da8154659c09d5ebddd2ad645d49a43fa0f218e1
srinivas163 <134271020+srinivas163@users.noreply.github.com>
2023-06-06 14:23:16 +0530

    n=int(input()) l=list(map(int,input().split())) a,b=map(int,input().split()) mi=99 c=0 for i in range(n):     if l[i]>=a and l[i]<=b:         if l[i]<mi:             mi=l[i]             c+=1 if c==0:     print("-1") else:     print(mi)

---
+++ Minimum_between_A_and_B.py
@@ -0,0 +1,14 @@
+n=int(input())
+l=list(map(int,input().split()))
+a,b=map(int,input().split())
+mi=99
+c=0
+for i in range(n):
+    if l[i]>=a and l[i]<=b:
+        if l[i]<mi:
+            mi=l[i]
+            c+=1
+if c==0:
+    print("-1")
+else:
+    print(mi)
